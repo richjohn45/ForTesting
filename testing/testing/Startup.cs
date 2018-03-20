61aff8ac0dca9059b81cff5cc0f950bf9b3704b2
richjohn45 <richardjohnson9876@gmail.com>
2018-03-20 15:32:58 -0400

    test
    testing

---
+++ testing/testing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testing.Startup))]
+namespace testing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
